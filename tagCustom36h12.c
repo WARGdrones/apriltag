5d28a88b3d3c8e97886167cfdb2e3da4346bde8d
poett1 <poett1@github.com>
2021-10-14 11:16:02 +0200

    added recursive tag custom36h12

---
+++ tagCustom36h12.c
@@ -0,0 +1,347 @@
+#include <stdlib.h>
+#include "tagCustom36h12.h"
+
+static uint64_t codedata[247] = {
+   0x0000000a3b6f275bUL,
+   0x0000000a75fe2d20UL,
+   0x0000000ab08d32e5UL,
+   0x0000000aeb1c38aaUL,
+   0x0000000b25ab3e6fUL,
+   0x0000000b603a4434UL,
+   0x0000000b9ac949f9UL,
+   0x0000000c4a765b48UL,
+   0x0000000cbf9466d2UL,
+   0x0000000cfa236c97UL,
+   0x0000000d34b2725cUL,
+   0x0000000d6f417821UL,
+   0x0000000e1eee8970UL,
+   0x0000000f092aa084UL,
+   0x0000000f43b9a649UL,
+   0x000000006884c322UL,
+   0x00000000dda2ceacUL,
+   0x00000002b21afcd4UL,
+   0x000000039c5713e8UL,
+   0x0000000536403c4bUL,
+   0x000000065b0b5924UL,
+   0x000000086a128d11UL,
+   0x00000009544ea425UL,
+   0x00000009c96cafafUL,
+   0x0000000a03fbb574UL,
+   0x0000000ab3a8c6c3UL,
+   0x0000000aee37cc88UL,
+   0x0000000e972828d8UL,
+   0x0000000ed1b72e9dUL,
+   0x0000000f0c463462UL,
+   0x0000000ff6824b76UL,
+   0x00000001906b73d9UL,
+   0x000000027aa78aedUL,
+   0x000000032a549c3cUL,
+   0x0000000364e3a201UL,
+   0x00000005e908e178UL,
+   0x00000008e24c2c79UL,
+   0x00000009cc88438dUL,
+   0x0000000d3ae99a18UL,
+   0x0000000db007a5a2UL,
+   0x0000000417ac472eUL,
+   0x0000000d03762831UL,
+   0x0000000dedb23f45UL,
+   0x0000000fc22a6d6dUL,
+   0x000000057a21fdaaUL,
+   0x00000006645e14beUL,
+   0x00000008736548abUL,
+   0x00000001da845cf4UL,
+   0x00000003e98b90e1UL,
+   0x00000002182ef697UL,
+   0x00000006e906039eUL,
+   0x0000000723950963UL,
+   0x00000007d3421ab2UL,
+   0x0000000c666e8e16UL,
+   0x00000006b4ae2595UL,
+   0x0000000d56e1cce6UL,
+   0x0000000f65e900d3UL,
+   0x00000000ffd22936UL,
+   0x00000008d023a8faUL,
+   0x0000000f7257504bUL,
+   0x000000039065b825UL,
+   0x0000000a6d28653bUL,
+   0x000000018dccd3b0UL,
+   0x0000000b9e61d18cUL,
+   0x0000000f4a6dc1baUL,
+   0x0000000b6d258761UL,
+   0x00000009d657f2dcUL,
+   0x0000000630832a6aUL,
+   0x00000003b5b0a68dUL,
+   0x00000005c7d36e58UL,
+   0x0000000820f38b60UL,
+   0x0000000b958c09a7UL,
+   0x0000000c4854aed4UL,
+   0x00000005e6e73504UL,
+   0x0000000241126c92UL,
+   0x000000041edd5654UL,
+   0x0000000668739006UL,
+   0x000000019dd3aabbUL,
+   0x00000006408a0175UL,
+   0x00000007da7329d8UL,
+   0x000000017905b008UL,
+   0x0000000cb1815e9bUL,
+   0x0000000c080b7508UL,
+   0x000000062b45ea18UL,
+   0x00000007c52f127bUL,
+   0x0000000e70b57566UL,
+   0x000000046faa5ae0UL,
+   0x000000052ee14f85UL,
+   0x0000000ac8420ccbUL,
+   0x00000001a8204dbfUL,
+   0x00000006b06accadUL,
+   0x00000007de88a520UL,
+   0x0000000401406ac7UL,
+   0x0000000320570f4dUL,
+   0x0000000fb5114ca0UL,
+   0x0000000495723cfdUL,
+   0x0000000f3d54a0edUL,
+   0x000000099b1e1bc2UL,
+   0x00000003abb3199eUL,
+   0x000000006c6c60caUL,
+   0x0000000e4b3ca95eUL,
+   0x00000000d2fa7068UL,
+   0x00000002e932b37bUL,
+   0x00000009a75e8d9aUL,
+   0x0000000c09d1663cUL,
+   0x000000036ed489c5UL,
+   0x00000001e80dcc4bUL,
+   0x00000002a9388fc4UL,
+   0x00000006b8b6fbb2UL,
+   0x0000000e146763a1UL,
+   0x00000009489fb94cUL,
+   0x00000004ac9d7e03UL,
+   0x00000007dab59ac2UL,
+   0x0000000d7b47672eUL,
+   0x00000003716684c3UL,
+   0x0000000e47e692b5UL,
+   0x0000000ddc98367aUL,
+   0x00000007edaa280bUL,
+   0x0000000e0cacbb9bUL,
+   0x0000000c9b792b93UL,
+   0x0000000c302acf58UL,
+   0x000000043b884896UL,
+   0x0000000dcb8ad2daUL,
+   0x0000000f46dd2846UL,
+   0x00000002e774b066UL,
+   0x0000000ecd7bc3d4UL,
+   0x0000000d77745cd0UL,
+   0x0000000faee21303UL,
+   0x0000000c30c8fb82UL,
+   0x0000000286c20d44UL,
+   0x0000000a953b1a60UL,
+   0x0000000b51567b27UL,
+   0x000000007e411728UL,
+   0x0000000973daa77aUL,
+   0x000000007a7ab1f5UL,
+   0x0000000205d3de6cUL,
+   0x00000003bd2c14bcUL,
+   0x000000089b30d5daUL,
+   0x0000000d5df4daaaUL,
+   0x0000000dd312e634UL,
+   0x0000000b6dc6ea9eUL,
+   0x0000000ca0f425c3UL,
+   0x0000000652f82cf1UL,
+   0x000000062238d67bUL,
+   0x00000000302f342eUL,
+   0x0000000be182ae53UL,
+   0x0000000074814417UL,
+   0x0000000d1b853d5eUL,
+   0x00000006a37e0987UL,
+   0x0000000aaaa3a145UL,
+   0x00000003aba4f47aUL,
+   0x00000009e98d7116UL,
+   0x0000000e2de0aec2UL,
+   0x0000000e5d6b9b03UL,
+   0x00000006485faaebUL,
+   0x0000000d153cb96bUL,
+   0x000000097958d8bdUL,
+   0x00000005e9173dbdUL,
+   0x0000000b6135e2b8UL,
+   0x0000000a87fa8a19UL,
+   0x0000000963ac60f5UL,
+   0x0000000c5aa021e2UL,
+   0x000000043d9718daUL,
+   0x00000005758c8ebbUL,
+   0x00000002d34d7002UL,
+   0x00000005bda503d7UL,
+   0x00000004088fb270UL,
+   0x00000008553bc44cUL,
+   0x0000000ac2246686UL,
+   0x000000077cbb1940UL,
+   0x0000000d75c7ed13UL,
+   0x0000000d956d3abeUL,
+   0x0000000f85c4515eUL,
+   0x000000086c50f20fUL,
+   0x0000000f609a9b6bUL,
+   0x0000000408e536abUL,
+   0x00000006f4648f8aUL,
+   0x00000009e5f06998UL,
+   0x0000000e74aba674UL,
+   0x000000081f0998e6UL,
+   0x0000000ee244d5b7UL,
+   0x0000000e46d54f30UL,
+   0x0000000aeb196fd9UL,
+   0x00000001d32a957cUL,
+   0x0000000f1e9515a4UL,
+   0x0000000bf2b338a3UL,
+   0x0000000eff6804a2UL,
+   0x000000077d973669UL,
+   0x0000000559116854UL,
+   0x0000000206ccb41fUL,
+   0x00000009d004ef1aUL,
+   0x0000000b0e6c0f82UL,
+   0x0000000ce61c52f1UL,
+   0x000000036c29e9d4UL,
+   0x00000009881c0eb8UL,
+   0x00000005b19e3a34UL,
+   0x0000000b140bb05bUL,
+   0x0000000d4dbc4b77UL,
+   0x0000000276f5158fUL,
+   0x0000000e6ea638ceUL,
+   0x000000066511e7d7UL,
+   0x0000000f6a59cb07UL,
+   0x0000000ecfdabdd2UL,
+   0x00000001e17c3415UL,
+   0x0000000eeb5fa224UL,
+   0x00000007574d3d37UL,
+   0x00000004455ce800UL,
+   0x00000007d082b244UL,
+   0x00000000634b2a13UL,
+   0x0000000de44d17abUL,
+   0x00000002ccbfb442UL,
+   0x00000005653a48deUL,
+   0x0000000f00415ae8UL,
+   0x0000000d08e25bb6UL,
+   0x000000079d2a4f35UL,
+   0x000000042947c775UL,
+   0x00000001352ba983UL,
+   0x00000002959dec0eUL,
+   0x00000000c2670467UL,
+   0x000000040bc382c9UL,
+   0x000000002bb5a70fUL,
+   0x0000000ad3ed8f3aUL,
+   0x0000000b34a9d795UL,
+   0x0000000ce5d60b5bUL,
+   0x0000000012335c18UL,
+   0x00000002dce7595cUL,
+   0x00000000d61e2f50UL,
+   0x0000000478480241UL,
+   0x0000000ce465a050UL,
+   0x00000002fed2c4f1UL,
+   0x0000000ef4baf70aUL,
+   0x00000009e2a16e30UL,
+   0x0000000d8860235fUL,
+   0x00000004be918841UL,
+   0x0000000319c8ed03UL,
+   0x00000008319d4f47UL,
+   0x00000004fa66c2b4UL,
+   0x0000000e7356e9f4UL,
+   0x0000000546298bcaUL,
+   0x0000000bb0be0adeUL,
+   0x00000004b42459e3UL,
+   0x0000000d1ef0d533UL,
+   0x0000000b4d1891faUL,
+   0x00000008c7e32491UL,
+   0x0000000343a08f27UL,
+   0x0000000cf25b5de6UL,
+   0x0000000b78eeb22aUL,
+};
+apriltag_family_t *tagCustom36h12_create()
+{
+   apriltag_family_t *tf = calloc(1, sizeof(apriltag_family_t));
+   tf->name = strdup("tagCustom36h12");
+   tf->h = 12;
+   tf->ncodes = 247;
+   tf->codes = codedata;
+   tf->nbits = 36;
+   tf->bit_x = calloc(36, sizeof(uint32_t));
+   tf->bit_y = calloc(36, sizeof(uint32_t));
+   tf->bit_x[0] = -2;
+   tf->bit_y[0] = -2;
+   tf->bit_x[1] = -1;
+   tf->bit_y[1] = -2;
+   tf->bit_x[2] = 0;
+   tf->bit_y[2] = -2;
+   tf->bit_x[3] = 1;
+   tf->bit_y[3] = -2;
+   tf->bit_x[4] = 2;
+   tf->bit_y[4] = -2;
+   tf->bit_x[5] = 3;
+   tf->bit_y[5] = -2;
+   tf->bit_x[6] = 4;
+   tf->bit_y[6] = -2;
+   tf->bit_x[7] = 5;
+   tf->bit_y[7] = -2;
+   tf->bit_x[8] = 6;
+   tf->bit_y[8] = -2;
+   tf->bit_x[9] = 7;
+   tf->bit_y[9] = -2;
+   tf->bit_x[10] = 7;
+   tf->bit_y[10] = -1;
+   tf->bit_x[11] = 7;
+   tf->bit_y[11] = 0;
+   tf->bit_x[12] = 7;
+   tf->bit_y[12] = 1;
+   tf->bit_x[13] = 7;
+   tf->bit_y[13] = 2;
+   tf->bit_x[14] = 7;
+   tf->bit_y[14] = 3;
+   tf->bit_x[15] = 7;
+   tf->bit_y[15] = 4;
+   tf->bit_x[16] = 7;
+   tf->bit_y[16] = 5;
+   tf->bit_x[17] = 7;
+   tf->bit_y[17] = 6;
+   tf->bit_x[18] = 7;
+   tf->bit_y[18] = 7;
+   tf->bit_x[19] = 6;
+   tf->bit_y[19] = 7;
+   tf->bit_x[20] = 5;
+   tf->bit_y[20] = 7;
+   tf->bit_x[21] = 4;
+   tf->bit_y[21] = 7;
+   tf->bit_x[22] = 3;
+   tf->bit_y[22] = 7;
+   tf->bit_x[23] = 2;
+   tf->bit_y[23] = 7;
+   tf->bit_x[24] = 1;
+   tf->bit_y[24] = 7;
+   tf->bit_x[25] = 0;
+   tf->bit_y[25] = 7;
+   tf->bit_x[26] = -1;
+   tf->bit_y[26] = 7;
+   tf->bit_x[27] = -2;
+   tf->bit_y[27] = 7;
+   tf->bit_x[28] = -2;
+   tf->bit_y[28] = 6;
+   tf->bit_x[29] = -2;
+   tf->bit_y[29] = 5;
+   tf->bit_x[30] = -2;
+   tf->bit_y[30] = 4;
+   tf->bit_x[31] = -2;
+   tf->bit_y[31] = 3;
+   tf->bit_x[32] = -2;
+   tf->bit_y[32] = 2;
+   tf->bit_x[33] = -2;
+   tf->bit_y[33] = 1;
+   tf->bit_x[34] = -2;
+   tf->bit_y[34] = 0;
+   tf->bit_x[35] = -2;
+   tf->bit_y[35] = -1;
+   tf->width_at_border = 6;
+   tf->total_width = 10;
+   tf->reversed_border = false;
+   return tf;
+}
+
+void tagCustom36h12_destroy(apriltag_family_t *tf)
+{
+   free(tf->bit_x);
+   free(tf->bit_y);
+   free(tf->name);
+   free(tf);
+}
---
+++ tagCustom36h12.h
@@ -0,0 +1,17 @@
+#ifndef _TAGCustom36H12
+#define _TAGCustom36H12
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tagCustom36h12_create();
+void tagCustom36h12_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
